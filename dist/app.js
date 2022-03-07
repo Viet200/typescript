7e9b7961d93357dd62491fac4f682376fa5e7d4d
vietnvph13644 <vietnvph13644@fpt.edu.vn>
2022-03-07 15:38:52 +0700

    cai typescript

---
+++ dist/app.js
@@ -0,0 +1,9 @@
+"use strict";
+const person = {
+    name: "dat",
+    age: 20
+};
+const persons = [
+    { id: 1, name: "dat", age: 20 },
+    { id: 2, name: "dai", age: 25 },
+];
---
+++ src/app.ts
@@ -0,0 +1,14 @@
+type Tperson = {
+    id?:number;
+    name:string;
+    age:number
+}
+const person: Tperson = {
+    name:"dat",
+    age:20
+}
+const persons:{id:number,name:string,age:number}[]=[
+    {id:1,name:"dat",age:20},
+    {id:2,name:"dai",age:25},
+
+]
